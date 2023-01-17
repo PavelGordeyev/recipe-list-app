04e25f084449ff2bb2602839f3ad92a1ede96a1f
PavelGordeyev <gordeyev.p@gmail.com>
2023-01-16 23:12:43 -0800

    Add RecipeFeaturedView and add it to RecipeTabView

---
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeFeaturedView.swift
+//  Recipe List App
+//
+//  Created by Pavel Gordeyev on 1/16/23.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
